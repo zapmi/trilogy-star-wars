ecfcf251909f3d32c6a7648018a8afe31d75baca
Mike Z <mzapata2991@gmail.com>
2019-06-04 21:24:03 -0500

    changed back to view

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ var characters = [
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
-app.get("/view", function(req, res) {
+app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "view.html"));
 });
 
